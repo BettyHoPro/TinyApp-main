87a0922ef163eafb4810108153dd9991c79d6d8a
BettyHoPro <bettyvisualtech@gmail.com>
2021-02-16 01:08:48 +0000

    submit URL Shortening part 1

--- express_server.js
+++ express_server.js
@@ -1,16 +1,56 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substr(2,6);
+};
+
+
+app.post('/urls', (req, res) => {
+  const shorURL = generateRandomString();
+  console.log(req.body);
+  res.send('OK');
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
+// app.get("/hello", (req, res) => {
+//   const templateVars = { greeting: 'Hello World!' };
+//   res.render("hello_world", templateVars);
+// });
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase};
+  res.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  res.redirect(urlDatabase[req.params.shortURL]);
+});
+
 app.get('/urls.json',  (req, res) => {
   res.json(urlDatabase);
 });
