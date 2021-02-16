704fbd08ce2bafe834ce3656230fafeb6e09ba0c
BettyHoPro <bettyvisualtech@gmail.com>
2021-02-16 21:40:24 +0000

    Add delete btn in index page

--- express_server.js
+++ express_server.js
@@ -25,6 +25,10 @@ app.post('/urls', (req, res) => {
   res.redirect(`urls/${shortURL}`);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello!');
